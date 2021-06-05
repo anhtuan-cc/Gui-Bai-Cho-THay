be35b84fe7ca7f353415f231bf2ebc48946b726f
anhtuan-cc <nguyenanhtuan.tc1991@gmail.com>
2021-06-05 15:41:53 +0700

    bai thi

---
+++ B2. chuyen sang chu in hoa.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+void upperCase(char s[]){
+	for(int i=0;i<strlen(s);i++){
+		if(s[i]>=97&&s[i]<=122){
+			s[i]-=32;
+		}
+	}
+    printf("Chuoi sau khi chuyen sang in hoa: %s",s);
+}
+int main(){
+	char s[50];
+	printf("Nhap chuoi s ");
+	scanf("%s",s);
+	upperCase(s);
+}
